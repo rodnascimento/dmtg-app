8c77801c5df705e3ee639ce5e5edbb976be8410e
Rodrigo do Nascimento Siqueira <107624409+rodnascimento@users.noreply.github.com>
2023-07-04 01:21:29 -0300

    Update App.js
    na mao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
+import React, {  useState } from "react";
 import './App.css';
+import api from "./service/api";
 
 function App() {
+  
+  const [steps, setSteps] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  const handleClick = () => {
+    console.log(userInput)
+    console.log(steps)
+    api
+      .post("/testStage/conversational_predict",{
+            inputs: userInput
+      })
+      .then((response) => {
+        setSteps([...steps, "Me: "+userInput, "Bot: "+response.data["body"][0]["generated_text"]])
+      })
+      .catch((err) => {
+        console.error("Vixe! ocorreu um erro" + err);
+      });
+  };
+  
+  const handleChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
       <h1>DMTG</h1>
       <hr/>
@@ -12,16 +36,25 @@ function App() {
           Utilizando o poder de um modelo essa mensagem ira hostear modelo pré-treinado para geração de text (GPT-like)
         </p>
         <p>
-          Exemplo: "Me dê uma ideia criativa para festa de criança"
+          Exemplo: "What is the capital of USA?"
         </p>
-        <form>
-          <input type="text" placeholder="Hello there" style={{width: "600px"}} />
+        <div>
           <hr/>
-          <input type="submit" value="RUN" />
-        </form>
+          {steps.map(( message ) => (
+            <p>{message}</p>
+          ))}
+          <hr/>
+        </div>
+          <input onChange={handleChange} value={userInput} type="text" placeholder="Hello there" style={{width: "600px"}} />
+          <hr/>
+          <button onClick={handleClick}  >RUN</button>
+      
+
       </header>
     </div>
   );
+
 }
+
 
 export default App;
